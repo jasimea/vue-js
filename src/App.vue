d5a258f9f9575e95cf3bd063d4ab11150efe45dd
Suhail Ea <suhailedayadi@gmail.com>
2020-10-18 11:07:57 +0530

    navbar,slidebar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+   
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Navbar />
+    <Sidebar/>
+    <Input />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue'
+import Sidebar from './components/Sidebar.vue'
+import Input from './components/Input.vue'
+
+
+
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,Navbar,Sidebar,Input
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Input.vue
@@ -0,0 +1,48 @@
+<template>
+  <form class="form">
+    <div class="form-group">
+    <label for="exampleInputEmail1">First Name</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Last Name</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+</template>
+<script>
+export default {
+  name:'Input'
+}
+</script>
+<style scoped>
+.form{
+  position: relative;
+    left: 13%;
+    width: 30%;
+    background: white;
+}
+
+.form-group {
+    width: 40%;
+    margin-top: 40px;
+    margin-left: 30%;
+}
+.btn{
+  margin-left: 30%;
+  
+}
+.check-box{
+  display: flex;
+}
+</style>
